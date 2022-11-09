670a2d1282979c06fe4f28e0bf9bfec841f9d5f5
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2022-11-09 22:08:45 +0530

    Time: 599 ms (73.75%), Space: 167.9 MB (57.37%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
-    int longestPalindrome(vector<string>& w) {
-        map<string,int>m1,m2;
-        int n = w.size();
-        for(int i =0 ; i<n ; i++){
-            if(w[i][0]==w[i][1]){
-                m2[w[i]]++;
-            }
-            else{
-                m1[w[i]]++;
-            }
+    
+    int longestPalindrome(vector<string>& words) {
+        int n=words.size();
+
+        map<string,int>mp;
+
+        for(auto i:words){
+            mp[i]++;
         }
-        int count =0;
-        for(auto i : m1){
-            // cout<<i.first<<" //"<<i.second<<endl;
-            string s = i.first;
-            string k = s;
-            reverse(k.begin(), k.end());
-            int a = m1[s];
-            int b =0;
-            if( m1.find(k)!=m1.end()){
-                b =m1[k];
-            }
-            count+= min(a,b)*4;
-            m1[s] =0;
-        }
-         // cout<<count<<" "<<endl;
-        int ev =0;
-        int maxi = 0;
-        for(auto i:m2){
-             // cout<<i.first<<" //"<<i.second<<endl;
-            if(i.second%2==0){
-                ev++;
-                count+=2*(i.second);
-            } 
-            else{
-                count += 4*(i.second/2);
-                maxi = max(maxi, i.second);
-            }
+        
+        int ans=0;
+        bool flag=false;
+        for(auto i:mp){
             
-      }
-        if(maxi%2 !=0){
-            count+=2;
+            string x=i.first;
+            string t=x;
+            reverse(t.begin(),t.end());
+            
+            if(t==x){
+                ans+=i.second/2;
+                if(i.second%2==1){
+                    flag=true;
+                }
+            }else{
+                
+                ans+=min(i.second,mp[t]);
+                mp.erase(t);
+            }
         }
-        return count;
+        
+        ans=ans*4;
+        if(flag){
+            ans+=2;
+        }
+        
+        return ans;
+        
+        
+        
     }
 };
