91a346ea1025b05828c7bb105929c38b5c677ae0
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2022-11-15 23:15:08 +0530

    Time: 75 ms (23.17%), Space: 31 MB (9.34%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,29 +11,61 @@
  */
 class Solution {
 public:
-    int cnt=1;
-    void solve(TreeNode* root){
-        if(root==NULL){
-            return;
-        }
-        
-        if(root->left){
-            cnt++;
-            solve(root->left);
-        }
-        
-        if(root->right){
-            cnt++;
-            solve(root->right);
-        }
-    }
-    int countNodes(TreeNode* root) {
-        
+    
+    int left(TreeNode* root){
         if(root==NULL){
             return 0;
         }
         
-        solve(root);
-        return cnt;
+        // int ans=0;
+        // if(root->left){
+        //     ans=left(root->left);
+        // }
+        // return 1+ans;
+        
+        return 1+left(root->left);
     }
+    
+    
+    int right(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        
+        // int ans=0;
+        // if(root->right){
+        //     ans=left(root->right);
+        // }
+        // return 1+ans;
+        
+        return 1+right(root->right);
+    }
+
+    int countNodes(TreeNode* root) {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        
+        cout<<"root->val "<<root->val<<endl;
+        int l,r=0;
+        if(root->left){
+            // l=1+height(root->left);//ush particular node ko bhi toh add karnge naa 
+            l=1+left(root->left);
+        }
+        
+        if(root->right){
+            r=1+right(root->right);
+        }
+        
+        if(l==r)
+        {
+            return pow(2,l)-1;
+        }
+        else
+        {
+            return 1+countNodes(root->left)+countNodes(root->right);
+        }
+    }
+    
 };
