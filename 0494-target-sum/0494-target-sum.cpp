3920f2bf6b7bd9ad9879598fdccd1a2ab92ab877
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2022-11-18 01:20:14 +0530

    Time: 22 ms (73.02%), Space: 12.1 MB (47.83%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, int i, int sum) 
+  
+    int solve(vector<int>&nums, int i, int sum,vector<vector<int>>&dp) 
     {   
        if(i==0){
             if(sum==0 && nums[0]==0){
@@ -13,24 +14,31 @@ public:
             
         }
         
-        int a=solve(nums, i-1, sum);// exclude
-        int b=0;
-        if(nums[i]<=sum){
-            b = solve(nums, i-1, sum-nums[i]);
+        if(dp[i][sum]!=-1){
+            return dp[i][sum];
         }
         
-        return a+b;
+        int a=solve(nums, i-1, sum,dp);// exclude
+        int b=0;
+        if(nums[i]<=sum){
+            b = solve(nums, i-1, sum-nums[i],dp);
+        }
+        
+        
+        return dp[i][sum]= a+b; 
         
        
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum= accumulate(nums.begin(), nums.end(),0);
         
-        int W=(sum+target)/2;
+        int W=(sum-target)/2;
         
-        if(sum<target || (sum+target) % 2!=0){
+        if(sum<target || (sum-target) % 2!=0){
             return 0;
         }
-        return solve(nums, nums.size()-1, W);
+        
+        vector<vector<int>>dp(nums.size(),vector<int>(W+1,-1));
+        return solve(nums, nums.size()-1, W , dp);//-100
     }
 };
