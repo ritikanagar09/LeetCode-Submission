ebad3552bbfd6a738f92488f7adb67bb4a066502
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-01-12 22:42:58 +0530

    Time: 175 ms (95.03%), Space: 60.2 MB (99.01%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> adjList;
-    int dfs(vector<bool>& hasApple,int node,int d,int prev)
-    {
-        int result=0,temp;
-        for(int &i:adjList[node])
-	    if(i!=prev)
-	    {
-	        temp=dfs(hasApple,i,d+1,node);
-	        if(temp)			//If child has apples it'll return a non zero result which is the distance traveled upto that node.
-		    result+=temp-d;
-	    }
-        return result||hasApple[node]?result+d:0;  //If nothing is added to result and current node doesnt have apple return 0 else return distances of children + current distance from root.
+    int solve(int node, vector<bool>&vis, vector<int>adj[], vector<bool>& hasApple){
+       
         
+        //base case 
+        
+        int time=0;
+        vis[node]=true;
+        
+      
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                int ans=solve(it, vis,adj, hasApple);
+                if(ans>0 || hasApple[it]){
+                    time+=2+ans;
+                }
+               
+            }
+        }
+        
+     
+        return time;
+   
     }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        adjList.resize(n);
-        for(vector<int> &e:edges)
-            adjList[e[0]].push_back(e[1]),adjList[e[1]].push_back(e[0]);
-        return dfs(hasApple,0,0,-1)*2;
+        
+       
+        vector<int>adj[n];
+       
+        for(int i=0;i<edges.size();i++){
+            
+            int u=edges[i][0];
+            int v=edges[i][1];
+           
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+           
+        }
+     
+        vector<bool>vis(n,false);
+        return solve(0, vis, adj, hasApple);
     }
 };
