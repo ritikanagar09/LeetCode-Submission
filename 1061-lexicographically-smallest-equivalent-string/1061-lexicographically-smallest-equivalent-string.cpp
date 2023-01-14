01e1a2141b4a051a7089cfaa49590ba0eea9effd
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-01-14 23:31:07 +0530

    Time: 1384 ms (6.42%), Space: 401.5 MB (5.50%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    char dfs(char ch,unordered_map<char,vector<char>>adj,vector<int>&vis ){
+       
+        for(int i=0;i<26;i++){
+            vis[ch-'a']=1;
+        }
+        char ch1=ch;
+        for(char it: adj[ch]){
+            if(vis[it-'a']!=1){
+                char temp=dfs(it,adj,vis);
+                if(temp<ch1){
+                    ch1=temp;
+                }
+            }
+        }
+        
+        return ch1;
+    }
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        
+        int n=s1.length();
+        int m=2*n;
+        unordered_map<char , vector<char>>adj;
+        
+        for(int i=0;i<n;i++){
+            char u=s1[i];
+            char v=s2[i];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        string ans;
+        
+        for(int i=0;i<baseStr.size();i++){
+            
+            char ch=baseStr[i];
+            vector<int>vis(26,0);
+            
+            char min=dfs(ch,adj,vis);
+            ans+=min;
+        }
+        return ans;
+    }
+};
