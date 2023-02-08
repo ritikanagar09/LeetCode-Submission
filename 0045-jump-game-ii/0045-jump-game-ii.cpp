c8529c71417766c2606b774b47b2771cc4ec5a0c
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-02-09 03:49:20 +0530

    Time: 15 ms (84.06%), Space: 16.5 MB (84.20%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+         int reach=0, count=0, last=0;
+        for(int i=0;i<nums.size()-1;i++){  
+            reach = max(reach, i+nums[i]); 
+            if(i==last){  
+                last = reach;  
+                count++; 
+            }
+        }
+        return count;  
+    }
+    
+};
