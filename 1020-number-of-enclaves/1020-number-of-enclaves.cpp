867d4c1188ea8a436348546a65df5fb04eda7b0b
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-04-08 03:17:51 +0530

    Time: 76 ms (70.29%), Space: 29.3 MB (62.13%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,40 +1,74 @@
 class Solution {
 public:
-    void dfs(int i , int j ,vector<vector<int>>&vis,vector<vector<int>>& grid,int& ans,bool &a ){
-         int n = grid.size();
-        int m = grid[0].size();
-        if( i==0 || j==0 ||i==n-1 || j==m-1){
-            a = false;
+            // ye question is same as no of closed island but have some diffrence 
+        // rerefce: -striver graphs
+//      toh isme agar koi island bpundar wale island se connected h toh ..voh bhi boundary
+//         se bahar walk in kar skta ..and we dont want this 
+//         we want only islands jo bahar nhi ja pa rhae hai 
+//         toh agar hum boundary wale islands se dfs karu or dekhu agar ishe koi atatch h toh tum hat jao..you will not be counted in answer 
+//         bche hue is the answer 
+        
+    int m, n; 
+    
+    void dfs(int i, int j,vector<vector<int>>& grid ){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0){
+            return;
         }
-        vis[i][j]=1;
-        ans++;
-        int dx[] = {1,0,-1,0};
-        int dy[] = {0,-1,0,1};
-        for(int ii =0; ii<4 ; ii++){
-            int xx = i + dx[ii];
-            int yy = j + dy[ii];
-            if(xx>=0 && yy>=0 && xx<n && yy<m && grid[xx][yy]==1 && vis[xx][yy]==-1){
-                dfs(xx,yy,vis,grid,ans,a);
-            }
-        }
+        
+        // bss visit hi toh karna h 
+        //jisse boundary wale attach koi bhi island agar h toh ushe final count me nhi rkhna hume 
+        
+        grid[i][j]=0;
+        
+        dfs(i,j-1,grid);
+        dfs(i,j+1,grid);
+        dfs(i-1,j,grid);
+        dfs(i+1,j,grid);
+        
+        
     }
     int numEnclaves(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int cnt =0;
-        vector<vector<int>>vis(n,vector<int>(m,-1));
-        for(int i =0; i<n ; i++){
-            for(int j =0; j<m ; j++){
-                if(vis[i][j]==-1 && grid[i][j]==1){
-                    int ans =0;
-                    bool a = true;
-                    dfs(i,j,vis,grid, ans,a );
-                    if(a){
-                        cnt +=ans;
-                    }
+        m=grid.size();
+        n=grid[0].size();
+        
+        
+        // toh boundary walo ko tarverse kate hai sabse pehle 
+        // ab ynh aja kar unhe 0 bna de rhae jo boundary wale se attach so that vo
+        // final count me naa aaye 
+        
+        //first row
+        for(int j=0;j<n;j++){
+            if(grid[0][j]==1)
+                dfs(0,j,grid);
+        }
+        
+        //first coloumn
+        for(int i=0;i<m;i++){
+            if(grid[i][0]==1)
+                dfs(i,0,grid);
+        }
+        
+        //last row
+        for(int j=0;j<n;j++){
+            if(grid[m-1][j]==1)
+                dfs(m-1,j,grid);
+        }
+        
+        //last column
+        for(int i=0;i<m;i++){
+            if(grid[i][n-1]==1)
+                dfs(i,n-1,grid);
+        }
+        
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    cnt++;
                 }
             }
         }
+        
         return cnt;
     }
 };
