bc495a5fd8027c3400fc6d52f7c0f631dbdfaf11
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-05-07 04:01:14 +0530

    Time: 46 ms (6.64%), Space: 13.1 MB (99.60%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -10,23 +10,27 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    v.push_back({i,j});
-                }
-            }
-        }
-        
-        for(auto el:v){
-            int r=el.first;
-            int c=el.second;
-            cout<<r<<" "<<c<<endl;
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    if(i==r || j==c){
-                        matrix[i][j]=0;
+                    for(int k=0;k<m;k++){
+                        for(int b=0;b<n;b++){
+                            if(matrix[k][b]!=0 && (k==i ||b==j)){
+                                matrix[k][b]=-1e8;
+                            }
+                        }
                     }
                 }
             }
         }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+               if(matrix[i][j]==-1e8){
+                   matrix[i][j]=0;
+               } 
+            }
+        }
+        
+        
+        
         
        
     }
