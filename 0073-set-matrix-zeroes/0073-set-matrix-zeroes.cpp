d79b85ad9ab68b6ec55403782d7b5a6c49cd58b3
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-05-07 02:22:44 +0530

    Time: 21 ms (15.93%), Space: 13.2 MB (74.15%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    
     int m,n;
-    void solve(vector<vector<int>>& matrix, int r,int c){
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==r || j==c){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-    }
+    
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> nmat=matrix;
         m=matrix.size();
         n=matrix[0].size();
         
+        vector<pair<int,int>>v;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    solve(nmat,i,j);
+                    v.push_back({i,j});
                 }
             }
         }
         
-        matrix=nmat;
+        for(auto el:v){
+            int r=el.first;
+            int c=el.second;
+            cout<<r<<" "<<c<<endl;
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(i==r || j==c){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+        
+       
     }
 };
