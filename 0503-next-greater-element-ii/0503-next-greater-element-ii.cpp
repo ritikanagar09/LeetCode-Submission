e2ee56483bb9cbb5f3c6e42911ec3d9e0e8dc379
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-05-27 04:45:00 +0530

    Time: 43 ms (41.01%), Space: 23.9 MB (77.36%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& arr) {
         int n=arr.size();
-        arr.resize(2*n);
+        // arr.resize(2*n);
         
-        for(int i=n;i<2*n;i++){
-            arr[i]=arr[i-n];
-        }
-        vector<int> ans(2*n);
+        // for(int i=n;i<2*n;i++){
+        //     arr[i]=arr[i-n];
+        // }
+        vector<int> ans(n);
         stack<int>st;
         
         st.push(INT_MAX);
 
         for(int i=(2*n)-1;i>=0;i--){
-            int x=arr[i];
+            int x=arr[i%n];
             while(!st.empty() && x>=st.top()){
                 st.pop();
             }
@@ -21,21 +21,21 @@ public:
             int y=st.top();
             st.push(x);
             if(y==INT_MAX){
-                ans[i]=-1;
+                ans[i%n]=-1;
             }else{
-                ans[i]=y;
+                ans[i%n]=y;
             }
         }
         
-        // return ans;
+        return ans;
         
-        vector<int>res(n);
-        for(int i=0;i<n;i++){
-            res[i]=ans[i];
-        }
+//         vector<int>res(n);
+//         for(int i=0;i<n;i++){
+//             res[i]=ans[i];
+//         }
         
-        return res;
-        // return ans;
+//         return res;
+//         // return ans;
     }
         
 };
