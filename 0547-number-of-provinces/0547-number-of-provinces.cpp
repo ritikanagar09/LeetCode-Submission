9edc86ed891380223786c3187f21dd134a0fda04
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-06-05 04:22:33 +0530

    Time: 28 ms (52.62%), Space: 13.6 MB (87.25%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    /*
-    
-    SC: O(N) + 0(N)-> FOR satck space and vis vector
-    TC: o(N) + O(V+2E)
-    
-    O(V+2E)==Complexity of overall dfs of graph 
-    
-    */
     int n;
-    void dfs(int node, vector<int>&vis,vector<vector<int>>& isConnected ){
-        vis[node]=1;
+    void dfs(int i, vector<int>&vis,vector<vector<int>>& isConnected ){
+        vis[i]=1;
+        
         
         for(int j=0;j<n;j++){
-            if(isConnected[node][j]){
-                if(!vis[j]){
+            if(isConnected[i][j]){
+                if(!vis[j])
                     dfs(j,vis,isConnected);
-                }
             }
+            
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         n=isConnected.size();
         vector<int>vis(n,0);
         int cnt=0;
-        for(int i=0;i<n;i++){// visiitng all no connected components 
+        for(int i=0;i<n;i++){
+            
             if(!vis[i]){
                 cnt++;
                 dfs(i,vis,isConnected);
             }
-            
-            
         }
         
         return cnt;
+        
     }
 };
