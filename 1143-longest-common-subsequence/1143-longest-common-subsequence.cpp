bbae1995e10ce9851ca60d34e322d1031d4859d3
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-06-08 00:43:05 +0530

    Time: 160 ms (5.08%), Space: 273.4 MB (11.95%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,32 +1,59 @@
 class Solution {
-public:    
-    int dp[1000 + 1][1000 + 1];
-    
-    int maxLen(string& s1, string& s2, int n, int m)
-    {
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < m + 1; j++) {
-               if (i == 0 || j == 0) {
-                   dp[i][j] = 0;
-               } 
-            }
+public:
+    int solve(int i, int j,string str1, string str2, vector<vector<int>>&dp ){
+        if(i==0 || j==0){
+            return 0;
         }
         
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < m + 1; j++) {
-                if (s1[i - 1] == s2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = max(0 + dp[i][j - 1], 0 + dp[i - 1][j]);
-            }
+        // match or not match 
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-
-        return dp[n][m];
+        
+        if(str1[i-1]==str2[j-1]){
+            return dp[i][j]=1+solve(i-1,j-1, str1,str2,dp);
+        }
+        
+        return dp[i][j]=max(solve(i-1,j,str1,str2,dp),solve(i,j-1,str1,str2,dp));
     }
+    int longestCommonSubsequence(string text1, string text2) {
         
-    int longestCommonSubsequence(string s1, string s2) 
-    {
-        memset(dp, -1, sizeof(dp));        
-        return maxLen(s1, s2, s1.length(), s2.length());
+        int m=text1.size();
+        int n=text2.size();
+        
+        // we do memoisation bcoz we have are having overlapping subproblems 
+        
+        // vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        // return solve(m,n,text1,text2,dp);
+//         
+        string str1=text1;
+        string str2=text2;
+        vector<vector<int>>dp(1000+1,vector<int>(1000+1,0));
+        
+//         int dp[1000 + 1][1000 + 1];
+        
+//         for (int i = 0; i < m + 1; i++) {
+//             for (int j = 0; j < n + 1; j++) {
+//                if (i == 0 || j == 0) {
+//                    dp[i][j] = 0;
+//                } 
+//             }
+//         }
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+
+                
+            }
+        }
+        
+        return dp[m][n];
+        
     }
 };
