9bd53d73bebd5fe61c4d5103fe3acf6e414a9d09
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-06-28 15:34:49 +0530

    Time: 301 ms (73.41%), Space: 114.6 MB (43.70%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& a, vector<int>& b, int k) {
-        map<pair<int,int>,int>m;
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-        pq.push({a[0]+b[0],0,0});
-        vector<vector<int>>ans;
-        while(k-- and pq.size()){
-            auto v = pq.top();
-            pq.pop();
-            int i = v[1],j=v[2];
-            if(j+1<b.size() and m.find({i,j+1})==m.end()){
-                 pq.push({a[i]+b[j+1],i,j+1});
-                  m[{i,j+1}]++;
-            } 
-            if(i+1<a.size() and m.find({i+1,j})==m.end()){
-                pq.push({a[i+1]+b[j],i+1,j});
-                m[{i+1,j}]++;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        vector<vector<int>> ans;
+        set<pair<int, int>> visited;
+
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>> minHeap;
+        minHeap.push({nums1[0] + nums2[0], {0, 0}});
+        visited.insert({0, 0});
+
+        while (k-- && !minHeap.empty()) {
+            auto top = minHeap.top();
+            minHeap.pop();
+            int i = top.second.first;
+            int j = top.second.second;
+
+            ans.push_back({nums1[i], nums2[j]});
+
+            if (i + 1 < m && visited.find({i + 1, j}) == visited.end()) {
+                minHeap.push({nums1[i + 1] + nums2[j], {i + 1, j}});
+                visited.insert({i + 1, j});
             }
-            cout<<endl;
-            ans.push_back({a[i],b[j]});
+
+            if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
+                minHeap.push({nums1[i] + nums2[j + 1], {i, j + 1}});
+                visited.insert({i, j + 1});
+            }
         }
+
         return ans;
     }
 };
