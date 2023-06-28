85028d7453d148b220b886ab787a54246a80e7d4
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-06-28 15:40:46 +0530

    Time: 338 ms (54.58%), Space: 123.1 MB (15.53%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,36 +1,52 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int m = nums1.size();
-        int n = nums2.size();
-
-        vector<vector<int>> ans;
-        set<pair<int, int>> visited;
-
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>> minHeap;
-        minHeap.push({nums1[0] + nums2[0], {0, 0}});
-        visited.insert({0, 0});
-
-        while (k-- && !minHeap.empty()) {
-            auto top = minHeap.top();
-            minHeap.pop();
-            int i = top.second.first;
-            int j = top.second.second;
-
-            ans.push_back({nums1[i], nums2[j]});
-
-            if (i + 1 < m && visited.find({i + 1, j}) == visited.end()) {
-                minHeap.push({nums1[i + 1] + nums2[j], {i + 1, j}});
-                visited.insert({i + 1, j});
+        int n=nums1.size();
+        int m=nums2.size();
+        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
+        set<pair<int,int>>s;
+        
+        
+        int isum=nums1[0]+nums2[0];
+        pq.push({isum,{0,0}});
+        s.insert({0,0});
+        
+        vector<vector<int>>res;
+        for(int cnt=0;(cnt<k && !pq.empty()) ;cnt++){
+            auto temp=pq.top();
+            
+            pq.pop();
+            pair<int,int>p=temp.second;
+            
+            vector<int>ans;
+            int i=p.first;
+            int j=p.second;
+            
+            ans.push_back(nums1[i]);
+            ans.push_back(nums2[j]);
+            
+            res.push_back(ans);
+            
+            // cout<<i<<" "<<j<<endl;
+            
+            if(i+1<n  && (s.find({i+1,j})==s.end())){
+                int sum1=nums1[i+1]+nums2[j];
+                pq.push({sum1,{i+1,j}});
+                s.insert({i+1,j});
             }
-
-            if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
-                minHeap.push({nums1[i] + nums2[j + 1], {i, j + 1}});
-                visited.insert({i, j + 1});
+            
+            
+            
+            if(j+1<m &&  (s.find({i,j+1})==s.end())){
+                int sum2=nums1[i]+nums2[j+1];
+                pq.push({sum2,{i,j+1}});
+                s.insert({i,j+1});
             }
+            
+            
         }
-
-        return ans;
+        
+        
+        return res;
     }
 };
