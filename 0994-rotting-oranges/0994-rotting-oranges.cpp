17554deed2997a6ab0526280127d666bf17335ea
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-07-02 17:44:19 +0530

    Time: 6 ms (75.15%), Space: 13.3 MB (22.24%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,76 +1,70 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
         
-        queue<pair<int,int>>q;
-        int n=grid.size();
-        int m=grid[0].size();
-        
-        bool flag=false;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        bool flag=true;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]!=0){
-                    flag=true;
+                    flag=false;
+                    
                 }
             }
         }
         
-        if(!flag){
+        if(flag){
             return 0;
         }
-        
-        vector<vector<int>>vis=grid;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        queue<pair<int,int>>q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
-                    // back gya ye bhi toh visit ho gye h naa
-                    vis[i][j]=2;
-                }else{
-                    vis[i][j]=0;
                 }
             }
         }
-        
-        
-        int cnt=-1;
+        vector<vector<int>>vis(m, vector<int>(n,0));
+        int time=-1;
         while(!q.empty()){
             int size=q.size();
             
             while(size--){
-                pair<int,int> front=q.front();
-                int x=front.first;
-                int y=front.second;
+                int x=q.front().first;
+                int y=q.front().second;
+                
                 q.pop();
-                // vis[x][y]=-1;
-                int dx[]={1,-1,0,0};
-                int dy[]={0,0,1,-1};
+                
+                
+                int dx[]={-1,0,1,0};
+                int dy[]={0,1,0,-1};
                 
                 for(int i=0;i<4;i++){
                     int nx=x+dx[i];
                     int ny=y+dy[i];
                     
-                    if(nx>=0 && nx<n && ny>=0 && ny<m && 
-                      grid[nx][ny]==1 && vis[nx][ny]!=2){
-                        vis[nx][ny]=2;
+                    if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==1 && !vis[nx][ny]){
+                        grid[nx][ny]=2;
+                        vis[nx][ny]=1;
                         q.push({nx,ny});
                     }
                 }
-                
-                
             }
             
-            cnt++;
+            time++;
         }
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]!=2 && grid[i][j]==1){
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
                     return -1;
                 }
             }
         }
         
-        return cnt;
+        
+        return time;
     }
 };
