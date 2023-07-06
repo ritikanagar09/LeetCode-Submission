91741cb4c23e1a2f930794cd231e456d3d4f5ecd
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-07-06 15:35:18 +0530

    Time: 350 ms (5.02%), Space: 36.7 MB (71.61%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,70 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-// reverse = true =before
-// reverse=false=next
-class BSTIterator {
-    bool reverse=true;
-public:
-    
-    stack<TreeNode*>st;
-    BSTIterator(TreeNode* root, bool isReverse) {
-        reverse=isReverse;
-        pushAll(root);
-    }
-
-    int next() {
-        TreeNode* topnode=st.top();
-        st.pop();
-        if(!reverse)
-            pushAll(topnode->right);// bhai saare right node ke extreme left ko daalegi naa
-        else
-            pushAll(topnode->left);
-        return topnode->val;
-    }
-    
-    bool hasNext() {
-        return !st.empty();
-    }
-    
-private: 
-    void pushAll(TreeNode* node){
-        while(node!=NULL){
-            st.push(node);
-            
-            if(!reverse)
-                node=node->left;
-            else
-                node=node->right;
-        }
-    }
-};
-
-
-
 class Solution {
 public:
-    bool findTarget(TreeNode* root, int k) {
-        if(!root)return false;
+    
+    void solve(TreeNode* root, vector<int>&path, int k, bool &flag){
         
-        BSTIterator l(root,false); // smallest value ->next
-        BSTIterator r(root, true); // greatest value ->before
+        if(root==NULL){
+            return;
+        }
         
-        int i=l.next();
-        int j=r.next();// ye hume value de rha h ..ush paricular next node ki 
+       path.push_back(root->val);
         
-        while(i<j){
-            if(i+j==k){
-                return true;
-            }else if(i+j<k){
-                i=l.next();// i++
-            }else{
-                j=r.next();// j--
+        for(int i=0;i<path.size()-1;i++){
+            if(path[i]+root->val==k){
+                flag=true;
+                return;
             }
         }
         
-        return false;
+       solve(root->left,path,k,flag);
+       solve(root->right,path,k,flag);
         
+         
+        
+        // path.pop_back();
+    }
+    bool findTarget(TreeNode* root, int k) {
+        
+        
+        bool flag=false;
+        vector<int>path;
+        solve(root,path, k,flag);
+        
+        return flag;
     }
 };
