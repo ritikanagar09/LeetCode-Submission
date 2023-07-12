57d035f4b0a4019cab301f21cc0efd9db4d0b0ef
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-07-12 16:18:25 +0530

    Time: 442 ms (23.73%), Space: 148.7 MB (30.23%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -3,45 +3,30 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
         
-        // 2 cheeze dhyaan rkhni h isme 
+        int n=nums.size();
         
-        // 1: out of range ko bahar nikal do  -> from front
-        // 2: muhse or current element se koi kam h toh ushe bahar nikal do -> from back 
+        int right=0;
+        int left=0;
+        priority_queue<pair<int,int>>pq;
         
-        
-        // deque have been maintained in such a way that the only desired or usefull elemnet 
-        // which will heps in having answer will be there in our dequeue 
-        deque<int>dq;
-        
-        
-        vector<int>res;
-        
-        for(int i=0;i<nums.size();i++){
+        vector<int>ans;
+        while(right < n){
             
-            
-            // 1 step
-            
-            if(!dq.empty() && (i-dq.front())==k){
-                dq.pop_front(); // sirf ek element hoga --> ek baari mein 
+            pq.push({nums[right],right});
+            if(right-left+1>=k){
+                while(pq.top().second < left){
+                    pq.pop();
+                }
+                
+                ans.push_back(pq.top().first);
+                left++;
+                
             }
             
-            while(!dq.empty() && nums[dq.back()] < nums[i]){
-                dq.pop_back(); // tumahra raja aa gya h ...ab tum bacho ki jarurut nhi h
-            }
-            
-            dq.push_back(i);
-            
-            // daalna k window maintain hone k baad hi karnege 
-            
-            if(i >= k-1){
-                res.push_back(nums[dq.front()]);
-            }
-
-            
-            
+            right++;
         }
         
-        return res;
+        return ans;
         
         
     }
