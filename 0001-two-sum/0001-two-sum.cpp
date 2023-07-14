33289f18810ff5a95b6374acd326e4f9f42c3a95
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-07-14 10:38:44 +0530

    Time: 19 ms (54.35%), Space: 10.9 MB (23.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,20 +4,25 @@ public:
         map<int,int>mpp;
         int n=nums.size();
         
-        for(int i=0;i<n;i++){
-            mpp[nums[i]]=i;
-        }
+//         for(int i=0;i<n;i++){
+           
+//         }
         
         vector<int>ans;
         
         for(int i=0;i<n;i++){
-            int left=target-nums[i];
             
-            if(mpp.find(left)!=mpp.end() && i!=mpp[left]){
-                ans.push_back(i);
+            
+            int left=target-nums[i];
+             
+            if(mpp.find(left)!=mpp.end()){
                 ans.push_back(mpp[left]);
+                ans.push_back(i);
+                
                 break;
             } 
+            
+            mpp[nums[i]]=i;
         }
         
         return ans;
