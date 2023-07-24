52126c9e66a457274747aa88a63dc43c3c1a80a7
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-07-24 15:38:32 +0530

    Time: 8 ms (62.23%), Space: 15.1 MB (30.59%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,52 +1,21 @@
 class Solution {
 public:
-    int n;
-    int solve(int indx, int buy, vector<int>& prices,vector<vector<int>>&dp){
-        if(indx==n){
+    
+int solve(int indx, int prev, vector<int>&arr){
+    
+        if(indx >=arr.size()){
             return 0;
         }
-        
-        if(dp[indx][buy]!=-1){
-            return dp[indx][buy];
+
+        int ans=0;
+        if(prev!=-1 && arr[indx]>arr[prev]){
+            ans=abs(arr[indx]-arr[prev]);
         }
-        
-        int profit=0;
-        if(buy){
-            int buykaro=-prices[indx]+solve(indx+1,0,prices,dp);
-            int skipkaro=0+solve(indx+1,1,prices,dp);
-            profit=max(buykaro,skipkaro);
-        }else{
-            int sellkaro=prices[indx]+solve(indx+1,1,prices,dp);
-            int skipkaro=0+solve(indx+1,0,prices,dp);
-            profit=max(sellkaro,skipkaro);
-        }
-        
-        return dp[indx][buy]=profit;
-    }
+
+        ans+=solve(indx+1,indx,arr);
+        return ans;
+}
     int maxProfit(vector<int>& prices) {
-        n=prices.size();
-        
-        vector<vector<int>>dp(n+1, vector<int>(2,0));
-        // return solve(0,1 ,prices,dp);
-        
-        for(int indx=n-1;indx>=0;indx--){
-            for(int buy=0;buy<=1;buy++){
-                int profit=0;
-                if(buy){
-                    int buykaro=-prices[indx]+dp[indx+1][0];
-                    int skipkaro=0+dp[indx+1][1];
-                    profit=max(buykaro,skipkaro);
-                }else{
-                    int sellkaro=prices[indx]+dp[indx+1][1];
-                    int skipkaro=0+dp[indx+1][0];
-                    profit=max(sellkaro,skipkaro);
-                }
-                
-            dp[indx][buy]=profit;
-            }
-            
-        }
-        
-        return dp[0][1];
+        return solve(0, -1,prices );
     }
 };
