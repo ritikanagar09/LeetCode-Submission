340e70d4e4df73af27393eb5931a21f5c4df84b6
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-07-26 18:05:15 +0530

    Time: 31 ms (44.41%), Space: 15.4 MB (27.24%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    int n;
-    void dfs(int i, vector<int>&vis,vector<vector<int>>& isConnected ){
-        vis[i]=1;
+    
+    void dfs(int node,vector<vector<int>>& graph, vector<int>&vis ){
+        vis[node]=1;
         
-        
-        for(int j=0;j<n;j++){
-            if(isConnected[i][j]){
-                if(!vis[j])
-                    dfs(j,vis,isConnected);
+        for(int i=0;i<graph.size();i++){
+            if(graph[node][i]==1 && !vis[i]){// matlab tum mere padosi ho 
+                dfs(i,graph,vis);
             }
-            
         }
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        n=isConnected.size();
+    int findCircleNum(vector<vector<int>>& graph) {
+        int n=graph.size();
+        
         vector<int>vis(n,0);
+        
         int cnt=0;
         for(int i=0;i<n;i++){
             
             if(!vis[i]){
+                dfs(i,graph,vis);
                 cnt++;
-                dfs(i,vis,isConnected);
             }
+            
+            
         }
         
         return cnt;
-        
     }
 };
