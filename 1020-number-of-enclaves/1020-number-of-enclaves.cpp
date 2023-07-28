026c05fb5690e914f5dd62f91f7d85fa44014b43
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-07-28 17:04:56 +0530

    Time: 63 ms (99.67%), Space: 30.3 MB (48.97%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,74 +1,65 @@
 class Solution {
 public:
-            // ye question is same as no of closed island but have some diffrence 
-        // rerefce: -striver graphs
-//      toh isme agar koi island bpundar wale island se connected h toh ..voh bhi boundary
-//         se bahar walk in kar skta ..and we dont want this 
-//         we want only islands jo bahar nhi ja pa rhae hai 
-//         toh agar hum boundary wale islands se dfs karu or dekhu agar ishe koi atatch h toh tum hat jao..you will not be counted in answer 
-//         bche hue is the answer 
-        
-    int m, n; 
-    
-    void dfs(int i, int j,vector<vector<int>>& grid ){
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0){
-            return;
-        }
-        
-        // bss visit hi toh karna h 
-        //jisse boundary wale attach koi bhi island agar h toh ushe final count me nhi rkhna hume 
-        
-        grid[i][j]=0;
-        
-        dfs(i,j-1,grid);
-        dfs(i,j+1,grid);
-        dfs(i-1,j,grid);
-        dfs(i+1,j,grid);
-        
-        
-    }
     int numEnclaves(vector<vector<int>>& grid) {
-        m=grid.size();
-        n=grid[0].size();
-        
-        
-        // toh boundary walo ko tarverse kate hai sabse pehle 
-        // ab ynh aja kar unhe 0 bna de rhae jo boundary wale se attach so that vo
-        // final count me naa aaye 
-        
-        //first row
-        for(int j=0;j<n;j++){
-            if(grid[0][j]==1)
-                dfs(0,j,grid);
-        }
-        
-        //first coloumn
-        for(int i=0;i<m;i++){
-            if(grid[i][0]==1)
-                dfs(i,0,grid);
-        }
-        
-        //last row
-        for(int j=0;j<n;j++){
-            if(grid[m-1][j]==1)
-                dfs(m-1,j,grid);
-        }
-        
-        //last column
-        for(int i=0;i<m;i++){
-            if(grid[i][n-1]==1)
-                dfs(i,n-1,grid);
-        }
-        
-        int cnt=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    cnt++;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
+        vector<vector<int>>ans=grid;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    
+                    if(grid[i][j]==1){
+                        // ans[i][j]=2;
+                        vis[i][j]=1;
+                        q.push({i,j});
+                    }
+                    
+                    
                 }
             }
         }
         
+        
+        
+        int dx[]={-1,0,1,0};
+        int dy[]={0,1,0,-1};
+        
+        while(!q.empty()){
+            
+            int x=q.front().first;
+            int y=q.front().second;
+            
+            q.pop();
+            
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                
+                if(nx>=0 && ny>=0 && nx<n && ny<m && !vis[nx][ny] && grid[nx][ny]){
+                    q.push({nx,ny});
+                    vis[nx][ny]=1;
+                    // ans[nx][ny]=2;
+                }
+            }
+            
+            
+        }
+        
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(!vis[i][j] && grid[i][j]==1)
+                    cnt++;
+            }
+            
+        }
+        
         return cnt;
+        
+        
     }
 };
