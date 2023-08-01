96bb4395823481883cc3e22e862f67101334d023
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-01 14:33:50 +0530

    Time: 23 ms (83.49%), Space: 17.9 MB (35.81%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    void solve(int i, int n ,int k, vector<int>&path,vector<vector<int>>&ans ){
-        if(path.size()==k ){
-                ans.push_back(path);
+    
+    void solve(int indx, int k,vector<int>&path, vector<vector<int>>&ans,int n ){
+        if(k==0){
+            ans.push_back(path);
             return;
         }
         
-        
-        for(int j=i;j<=n;j++){
-            path.push_back(j);
-            
-            solve(j+1,n,k,path,ans);
-            path.pop_back();
-            
+        if(indx > n){
+            return;
         }
+        // cout<<"//"<<endl;
         
         
+        path.push_back(indx);
+        solve(indx+1,k-1,path,ans,n);
+        path.pop_back();
+        solve(indx+1,k,path,ans,n);
     }
     vector<vector<int>> combine(int n, int k) {
-        
         vector<vector<int>>ans;
         vector<int>path;
-        solve(1,n,k,path,ans);
+    
+        solve(1,k,path,ans,n);
         return ans;
         
     }
