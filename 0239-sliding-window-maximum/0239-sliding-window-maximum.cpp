af957632cf4d1dada5be74b769186626bb364337
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-02 20:37:35 +0530

    Time: 259 ms (94.90%), Space: 148.8 MB (28.59%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
         
-        deque<int>dq;
+        int left=0;
+        int right=0;
+        priority_queue<pair<int,int>>pq;
         vector<int>ans;
-        // we are storing indexes in deque
-        for(int i=0;i<n;i++){
+        while(right<nums.size()){
+            pq.push({nums[right],right});
             
-            // remove out of bounds 
-            
-            if(!dq.empty() && dq.front()==i-k){
-                dq.pop_front();
+            if(right- left + 1 >=k){
+                
+                while(pq.top().second < left){
+                    pq.pop();
+                }
+                
+                ans.push_back(pq.top().first);
+                left++;
             }
             
-            // we need to store only maximum elem
-            // store which are smaller than it 
-            // we have stored elem in decreasing order 
-            // toh sabse pehle piche remove krna 
-            // shuru karo
-            
-            while(!dq.empty() && nums[dq.back()] < nums[i]){
-                dq.pop_back();
-            }
-            
-            
-            dq.push_back(i);
-            
-            // we start taking elemnts from
-            
-            if(i>=k-1){
-                ans.push_back(nums[dq.front()]);
-            }
+            right++;
         }
         
         return ans;
+        
+        
     }
 };
