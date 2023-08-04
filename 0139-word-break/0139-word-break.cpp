2f33ccda9cde54ce966fe3a285dc652d548cb592
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-04 20:01:29 +0530

    Time: 47 ms (9.42%), Space: 29.5 MB (5.00%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
-    int dp[301];
-    bool solve(int indx, string s, map<string,int>& dict ){
+    
+    bool solve(int indx, string s, map<string,int>mpp,vector<int>&dp){
         
-        if(indx>=s.size()){
+        if(indx >=s.size()){
             return true;
         }
         
         if(dp[indx]!=-1){
             return dp[indx];
         }
-        
         for(int i=indx;i<s.size();i++){
-            string sub=s.substr(indx,i-indx+1);
-            
-            if(dict.find(sub)!=dict.end()){
-                // return solve(i+1,s,dict); 
-                
-                if(solve(i+1,s,dict)){
-                    return dp[i]=1; // this line is ensuirng ki agar true ata h toh ..tabhi rteurn true krna 
-                    // this means -> if false ata h toh return nothing and try exploring aage k paths 
-                    // they might give us true
-                    // but agar pehle hi false krva doge toh ..how woll try other ways also 
+            string str=s.substr(indx,i-indx+1);
+            if(mpp.find(str)!=mpp.end()){
+                if(solve(i+1,s,mpp,dp)){
+                    return dp[i]=true;
                 }
-                    
-                
             }
         }
         
-        
         return dp[indx]=false;
-        
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        
         map<string,int>mpp;
-        memset(dp,-1,sizeof dp);
-        
+        int n=wordDict.size();
         for(auto str:wordDict){
-            // st.insert(str);
             mpp[str]=1;
         }
-        
-        
-        return solve(0,s,mpp);
+        vector<int>dp(301,-1);
+        return solve(0,s,mpp,dp);
     }
 };
