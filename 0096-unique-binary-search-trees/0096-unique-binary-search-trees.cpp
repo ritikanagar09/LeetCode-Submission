e34298384eb98fdd6615ddfcce4c2835b4093bb2
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-05 18:11:58 +0530

    Time: 2264 ms (10.25%), Space: 5.9 MB (73.08%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    
-    int solve(int n){
+    int numTrees(int n) {
         
         if(n==0 || n==1){
             return 1;
         } 
-        
-        
+    
         int ways=0;
+        
+        // ye saari ki saari root nodes bna di h humne 
         for(int i=1;i<=n;i++){
-            ways+=solve(i-1)*solve(n-i);
+            ways+=numTrees(i-1)*numTrees(n-i);
         }
         
         return ways;
-    }
-    int numTrees(int n) {
-        
-        return solve(n);
     }
 };
