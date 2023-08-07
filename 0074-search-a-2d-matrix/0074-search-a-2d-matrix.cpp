2e13bb8faf50dfea584b892f6c0ac0154030af87
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-07 22:56:02 +0530

    Time: 3 ms (82.89%), Space: 9.4 MB (52.82%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,21 +4,17 @@ public:
         
         int m=matrix.size();
         int n=matrix[0].size();
+        int i=0;
+        int j=n-1;
         
-        
-        int low=0;
-        int high=m*n-1;
-        
-        while(low <= high){
-            int mid=(low+high)/2;
-            int i=mid/n;
-            int j=mid%n;
-            if(matrix[i][j]==target){
-                return true;
-            }else if(matrix[i][j] < target){
-                low=mid+1;
+        while(i < m && j>=0){
+            
+            if(target > matrix[i][j]){
+                i++;
+            }else if(target < matrix[i][j]){
+                j--;
             }else{
-                high=mid-1;
+                return true;
             }
         }
         
