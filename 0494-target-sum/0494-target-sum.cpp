a12fc2dcb76922e78c8b28d5571103a24df1e38e
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-09 01:51:35 +0530

    Time: 375 ms (33.74%), Space: 8.9 MB (86.85%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,54 @@
 class Solution {
 public:
-    int f(int i,int n , int k ,vector<int>& nums){
-        if(i>=n && k==0){
+    
+    int solve(int indx, int tar,vector<int>& nums){
+        
+        
+//         if(indx==0){
+//             if(tar==0 && nums[0]==0){
+//                 return 2;
+//             }else if(tar==0 || tar==nums[0]){
+//                 return 1;
+//             }
+            
+//             return 0;
+//         }
+        if(tar==0 && indx<0){
             return 1;
         }
-        if(i>=n || k<0){
+        if(indx<0 || tar<0){
             return 0;
         }
-        int pick = f(i+1,n,k-nums[i],nums);
-        int npick =f(i+1,n,k,nums);
-        return pick + npick;
+        
+        
+        
+        
+        int take=0;
+        // if(nums[indx] <= tar){
+            take=solve(indx-1,tar-nums[indx],nums);
+        // }
+        
+        int nottake=solve(indx-1,tar,nums);
+        
+        return take+nottake;
     }
-    int findTargetSumWays(vector<int>& nums, int tar) {
-        int n = nums.size();
-        int sum =0;
-        for(int i =0; i<n ; i++){
-            sum+= nums[i];
+    
+    
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
         }
-        int k = sum-tar;
-        if(k%2 !=0){
+        
+        int tar=(sum-target)/2;
+        
+        if((sum-target) < 0 || (sum-target)%2 != 0){
+            cout<<"//"<<endl;
             return 0;
         }
-        k = k/2;
-        int i =0;
-        int ans =f(i,n,k,nums);
-        return ans;
+        return solve(n-1,tar,nums);
+        
     }
 };
