41e29a3af58e5f32e41ada3911a9425b8d50eb2f
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-09 01:43:31 +0530

    Time: 273 ms (37.96%), Space: 9 MB (86.85%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,44 +1,54 @@
 class Solution {
 public:
-  
-    int solve(vector<int>&nums, int i, int sum,vector<vector<int>>&dp) 
-    {   
-       if(i==0){
-            if(sum==0 && nums[0]==0){
+    
+    int solve(int indx, int tar,vector<int>& nums){
+        
+        
+        if(indx==0){
+            if(tar==0 && nums[0]==0){
                 return 2;
-            }else if(sum==0 || nums[0]==sum){
+            }else if(tar==0 || tar==nums[0]){
                 return 1;
-            }else{
-                return 0;
             }
             
-        }
-        
-        if(dp[i][sum]!=-1){
-            return dp[i][sum];
-        }
-        
-        int a=solve(nums, i-1, sum,dp);// exclude
-        int b=0;
-        if(nums[i]<=sum){
-            b = solve(nums, i-1, sum-nums[i],dp);
-        }
-        
-        
-        return dp[i][sum]= a+b; 
-        
-       
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum= accumulate(nums.begin(), nums.end(),0);
-        
-        int W=(sum-target)/2;
-        
-        if(sum<target || (sum-target) % 2!=0){
             return 0;
         }
+        // if(tar==0){
+        //     return 1;
+        // }
+        // if(indx>=nums.size()){
+        //     return 0;
+        // }
         
-        vector<vector<int>>dp(nums.size(),vector<int>(W+1,-1));
-        return solve(nums, nums.size()-1, W , dp);//-100
+        
+        
+        
+        int take=0;
+        if(nums[indx] <= tar){
+            take=solve(indx-1,tar-nums[indx],nums);
+        }
+        
+        int nottake=solve(indx-1,tar,nums);
+        
+        return take+nottake;
+    }
+    
+    
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        
+        int tar=(sum-target)/2;
+        
+        if((sum-target) < 0 || (sum-target)%2 != 0){
+            cout<<"//"<<endl;
+            return 0;
+        }
+        return solve(n-1,tar,nums);
+        
     }
 };
