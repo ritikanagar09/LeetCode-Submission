337181640fc7f576835238d274e2981e3cb04ba8
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-13 01:17:00 +0530

    Time: 5 ms (28.33%), Space: 7.9 MB (25.64%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,29 +1,61 @@
 class Solution {
 public:
-    int solve(int i, int j , int m, int n,vector<vector<int>>& obstacleGrid, vector<vector<int>>&dp){
+    
+//     int solve(int i, int j,vector<vector<int>>& grid , int m, int n){
         
-        if(i>=m || j>=n || obstacleGrid[i][j]==1){
+//         if(i>=m || j>=n || grid[i][j]==1){
+//             return 0;
+//         }
+        
+//         if(i==m-1 && j==n-1){
+//             return 1;
+//         }
+        
+//         int right=solve(i,j+1,grid,m,n);
+//         int down=solve(i+1,j,grid,m,n);
+        
+//         return right+down;
+//     }
+    
+    int solve(int i, int j,vector<vector<int>>& grid , int m, int n){
+        
+        if(i<0 || j<0 || grid[i][j]==1){
             return 0;
         }
         
-        if(i==m-1 && j==n-1){
+        if(i==0 && j==0){
             return 1;
         }
         
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int down=solve(i+1,j,m,n,obstacleGrid,dp);
-        int right=solve(i,j+1,m,n,obstacleGrid,dp);
+        int right=solve(i,j-1,grid,m,n);
+        int down=solve(i-1,j,grid,m,n);
         
-        return dp[i][j]=down+right;
+        return right+down;
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
         
-        int m=obstacleGrid.size();
-        int n=obstacleGrid[0].size();
         
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(0,0,m,n,obstacleGrid,dp);
+        // return solve(0,0,grid,m,n);
+        
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        
+        dp[0][1]=1;
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                
+                if(grid[i-1][j-1]==0){
+                    int right=dp[i][j-1];
+                    int down=dp[i-1][j];
+
+                    dp[i][j]=right+down;
+                }
+                
+            }
+        }
+        
+        return dp[m][n];
     }
 };
