5b83bd88931767d50d03a8c7f82060e591b6df35
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-13 23:49:51 +0530

    Time: 2 ms (44.19%), Space: 8.3 MB (51.69%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -15,74 +15,23 @@ public:
         if(root==NULL){
             return {};
         }
+        
+        stack<TreeNode*>st;
         vector<int>ans;
-        
-        TreeNode* curr=root;
-        
-        while(curr != NULL){
-            // 2 cases can be there 
+        while(true){
             
-            if(curr->left==NULL){
-                // ya toh left tree ho gaa hi nhi ush left tree kaa
-                ans.push_back(curr->val);
-                curr=curr->right;
+            if(root != NULL){
+                st.push(root);
+                root=root->left;
             }else{
+                if(st.empty())break;
+                root=st.top();
+                st.pop();
+                ans.push_back(root->val);
+                root=root->right;
                 
-                // agar uska left part exit krta hai 
-                
-                // toh bhai jao left tree right most node par or 
-                
-                //usko point krvao apni current node par 
-                
-                // pointer to traverse to the rightmost node 
-                
-                
-                
-                
-                
-                
-                // YHNHA KAR DI NAA MISTAKE
-                
-                // LEFT TREE KE RIGHTMOST NODE PAR JANA HAI
-                // TreeNode* ptr=curr
-                
-                TreeNode* ptr=curr->left;
-                
-                
-                
-                
-                
-                
-                while(ptr->right && ptr->right !=curr){ // jab tak ptr kaa right exit kar rha h and and aisa naa ho thread exxit kr rha ho or tum right most child ko dhundne jaa rahe ho
-                    ptr=ptr->right;
-                    
-                }
-                
-                // now 2 cases will arrive 
-                // yaa toh ptr kaa right NULL hoga 
-                // yaa usme kuch hoga 
-                
-                
-                // see ptr ke right ke baare you will get to know only when jab tak tum uske pass pohoch nhi jaate 
-                // vahi jaa kar toh pta chalega 
-                if(ptr->right==NULL){
-                    ptr->right=curr;
-                    // now thread lag gya 
-                    // mujhe pta h mujhe knh avaaps aana h 
-                    curr=curr->left;
-                }else{
-                    
-                    
-                    // matlab thread exit kar rha h
-                    // that means we have already 
-                    // been traversed the left part 
-                    ptr->right=NULL;
-                    ans.push_back(curr->val);
-                    curr=curr->right;
-                }
             }
         }
-        
         
         return ans;
     }
