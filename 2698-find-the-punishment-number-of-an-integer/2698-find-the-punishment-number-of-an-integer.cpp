41e61e25858542f0812a6731d751a23a390d36d7
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-24 02:14:19 +0530

    Time: 465 ms (9.76%), Space: 5.9 MB (49.74%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -1,56 +1,48 @@
 class Solution {
 public:
-    bool solve(int indx, string &s, int curr, int sum){
-        
-        if(indx>=s.size()){
-            return curr==sum;
+   // 12 3456 
+    bool f(string kk,int a,int ans){
+        //cout<<kk<<" "<<a<<" "<<ans<<endl;
+        if(kk.size()==0  && ans == a){
+            //cout<<a<<" "<<" true "<<endl;
+            return true;
+        }
+        else if( kk.size()==0){
+            return false;
         }
         
-        if(curr>  sum )return false;
-        
-        
-        
-        
-        for(int i=indx;i<s.size();i++){
-            string str=s.substr(indx,i-indx+1);
-            int k=stoi(str);
-            
-            // cout<<k<<" "<<sum<<endl;
-            if(solve(i+1,s,curr+k,sum)){
-                return true;
-            }
-            
-            // sum+=k;
-             
+        for(int i =0; i<kk.size() ; i++){
+             string x = kk.substr(0,i+1);
+             if(x.size()>0){
+                 ans += stoi(x);
+                 string ss = kk.substr(i+1);
+                  bool t = f(ss,a,ans);
+                  ans -= stoi(x);
+                  if(t==true){
+                   return true;
+                }
+        }    
         }
-        
         return false;
     }
     int punishmentNumber(int n) {
         
-//         if(n <=8){
-//             return 1;
-//         }else if(n <=10){
-//             return 182;
-//         }else if(n<=40){
-//             return 1478;
-//         }
-                 
-//         int sum=1478;
-        int sum=0;
-        // string s="1296";
-        // int ans=solve(0,s,0,36);
-        // cout<<"//"<<ans<<endl;
-        for(int i=1;i<=n;i++){
-            
-            string s=to_string(i*i);
-            // cout<<"//"<<endl;
-            if(solve(0,s,0,i)){
-                sum+=i*i;
-                // cout<<i<<endl;
+        int ans1 =0;
+        int y = 0;
+        for(int i =0; i<=n ; i++){
+            int k = pow(i,2);
+            string kk = to_string(k);
+            int ans =0;
+            y =i;
+            if(kk=="100" || kk=="10000" || kk== "10000"){
+                y=1;
+            }
+         bool t = f(kk,y,0);
+            //cout<<t<<"//"<<kk << i <<endl;
+            if(t){
+                ans1+=k;
             }
         }
-                 
-        return sum;
+        return ans1;
     }
 };
