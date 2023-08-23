66960a97816f4a1b3f362d0002f204cb8f6fcadf
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-24 02:17:38 +0530

    Time: 481 ms (9.13%), Space: 5.9 MB (89.72%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
    // 12 3456 
     bool f(string kk,int a,int ans){
-        //cout<<kk<<" "<<a<<" "<<ans<<endl;
         if(kk.size()==0  && ans == a){
-            //cout<<a<<" "<<" true "<<endl;
             return true;
         }
         else if( kk.size()==0){
@@ -13,14 +11,14 @@ public:
         
         for(int i =0; i<kk.size() ; i++){
              string x = kk.substr(0,i+1);
-             if(x.size()>0){
+             // if(x.size()>0){
                  ans += stoi(x);
                  string ss = kk.substr(i+1);
                   bool t = f(ss,a,ans);
                   ans -= stoi(x);
                   if(t==true){
                    return true;
-                }
+                // }
         }    
         }
         return false;
@@ -32,13 +30,7 @@ public:
         for(int i =1; i<=n ; i++){
             int k = pow(i,2);
             string kk = to_string(k);
-            int ans =0;
-            y =i;
-            // if(kk=="100" || kk=="10000" || kk== "10000"){
-            //     y=1;
-            // }
-         bool t = f(kk,y,0);
-            //cout<<t<<"//"<<kk << i <<endl;
+            bool t = f(kk,i,0);
             if(t){
                 ans1+=k;
             }
