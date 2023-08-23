f45657cd0f45cf9c40415bfdc96e40202abe570b
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-24 02:31:53 +0530

    Time: 372 ms (30.33%), Space: 5.8 MB (89.72%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-   // 12 3456 
-    bool f(string kk,int a,int ans){
-        if(kk.size()==0){
-            return ans==a;
+    
+    bool canpart(string s, int target){
+        if(s=="" && target==0){
+            return true;
         }
         
-        if(ans > a){
+        if(target<0){
             return false;
         }
-        for(int i =0; i<kk.size() ; i++){
-                string x = kk.substr(0,i+1);
-                 ans += stoi(x);
-                 string ss = kk.substr(i+1);
-                  bool t = f(ss,a,ans);
-                  ans -= stoi(x);
-                  if(t==true){
-                        return true;
-                    }    
+        
+        
+        for(int i=0;i<s.size();i++){
+            string left=s.substr(0,i+1);
+            string right=s.substr(i+1);
+            int leftnum=stoi(left);
+            
+            bool flag=canpart(right,target-leftnum);
+            if(flag){
+               return true;
+            }
         }
+        
         return false;
     }
     int punishmentNumber(int n) {
         
-        int ans1 =0;
-        int y = 0;
-        for(int i =1; i<=n ; i++){
-            int k = pow(i,2);
-            string kk = to_string(k);
-            bool t = f(kk,i,0);
-            if(t){
-                ans1+=k;
+        
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(canpart(to_string(i*i),i)){
+                sum+=i*i;
             }
         }
-        return ans1;
+        
+        return sum;
+        
     }
 };
