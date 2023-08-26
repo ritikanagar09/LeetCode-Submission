4876d210f6fee2985f5f1756a37f884c2204c0d5
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-26 06:29:25 +0530

    Time: 223 ms (52.84%), Space: 65.2 MB (24.41%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
-    int solve(int prev, int curr, vector<vector<int>>& pairs,vector<vector<int>>&dp ){
-        
-        if(curr==pairs.size()){
-            return 0;
-        }
-        
-        if(dp[prev+1][curr]!=-1){
-            return dp[prev+1][curr];
-        }
-        
-        int nottake=solve(prev,curr+1,pairs,dp);
-        
-        int take=INT_MIN;
-        if(prev ==-1 || pairs[curr][0] > pairs[prev][1]){ // prev==-1 indeictaes if there is not previous lement and then you need to include the first elemnt that is present in the array 
-            take=1+solve(curr,curr+1,pairs,dp);
-        }
-        
-        return dp[prev+1][curr]=max(nottake,take);
-    }
     int findLongestChain(vector<vector<int>>& pairs) {
-        int n=pairs.size();
-        sort(pairs.begin(),pairs.end());
-        
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(-1,0,pairs,dp );
+        // THIS PROBELM CAN SOLVED USING EARLIEST DEADLINE FIRST ALGORITHM 
+        // IN WHICH THE WORK WHICH WOULD BE FINSHED FIRST SHOULD BE DONE FIRST ALSO
+        int n = pairs.size(), ctr = 1;
+	sort(pairs.begin(), pairs.end(), [&] (auto a, auto b) {return a[1] < b[1];});       // Sort the input matrix with respect to second element
+	int prev = pairs[0][1];
+	for(int i = 1; i < n; ++i){
+		if(pairs[i][0] > prev){
+			ctr++;
+			prev = pairs[i][1];
+		}
+	}
+	return ctr;
     }
 };
