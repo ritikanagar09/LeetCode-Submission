96ccdf38f6df501ee53753302fd8ff141dd1db68
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-08-26 05:51:57 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (58.66%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,63 +1,29 @@
 class Solution {
-    unordered_map<string , bool> m;
-    bool check(string s1, string s2, string s3,int l1, int l2, int l3, int p1, int p2, int p3){
-        // base case
-        if(p3==l3)
-            if(p1==l1 && p2==l2)
-                return true;
-            else
-                return false;
-        
-        
-        string key =to_string(p1)+"*"+to_string(p2)+"*"+to_string(p3);
-        // checking whther the key is already present or not 
-        if(m.find(key)!=m.end())// matab key mil gyi h
-            return m[key];
-        
-        
-        //NOW THERE WILL BE THREE CONDITION
-        
-        // COND 1:
-        // kya s1 puri complete ho gyi hai 
-        if(p1==l1)
-            if(s2[p2]==s3[p3])
-                return m[key]=check(s1,s2,s3,l1,l2,l3,p1,p2+1,p3+1);
-            else
-                return m[key]=false;
-        
-        // COND 2:
-        
-        if(p2==l2)
-            if(s1[p1]==s3[p3])
-                return m[key]=check(s1,s2,s3,l1,l2,l3,p1+1,p2,p3+1);
-            else
-                return m[key]=false;
-        
-        //  COND 3:
-        // AGAR DONO STRING PURI NHI HUI H ABHI TAKK
-        
-        // there might be possiblity ki humne 1st string se shuru kiya ho and may be second string se kiya ho 
-        bool one=false; 
-        bool two=false;
-        
-        if(s1[p1]==s3[p3])
-            one=check(s1,s2,s3,l1,l2,l3,p1+1,p2,p3+1);
-        
-        if(s2[p2]==s3[p3])
-            two=check(s1,s2,s3,l1,l2,l3,p1,p2+1,p3+1);
-        
-        return m[key]=one|| two;
-    
-    }
 public:
-    bool isInterleave(string s1, string s2, string s3) {
-        int l1=s1.length();
-        int l2=s2.length();
-        int l3=s3.length();
-        if((l1+l2)!=l3){
-            return false;
-        }else{
-            return check(s1,s2,s3,l1,l2,l3,0,0,0);
+    
+    bool solve(int i, int j, int k, string& s1, string& s2, string& s3, vector<vector<int>>&dp){
+        
+        if(i==s1.size() && j==s2.size() && k==s3.size()){
+            return true;
         }
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i]==s3[k] && s2[j]==s3[k]){
+            return dp[i][j]=solve(i+1,j,k+1,s1,s2,s3,dp) || solve(i,j+1,k+1,s1,s2,s3,dp);
+        }else if(s1[i]==s3[k]){
+            return dp[i][j]=solve(i+1,j,k+1,s1,s2,s3,dp);
+        }else if(s2[j]==s3[k]){
+            return dp[i][j]=solve(i,j+1,k+1,s1,s2,s3,dp);
+        }
+        
+        return dp[i][j]=false;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s3.size() != s1.size() + s2.size())
+			return false;
+        vector<vector<int>>dp(s1.size()+1, (vector<int>(s2.size()+1, -1)));
+        return solve(0,0,0,s1,s2,s3,dp);
     }
 };
