5f405ff1110d69d2ddc69d84015f326d445c7126
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-10-16 06:01:38 +0530

    Time: 11 ms (82.86%), Space: 14 MB (34.27%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,14 +1,58 @@
+// class Solution {
+// public:
+//     long long int mod=1e9+7;
+//     long long solve(int indx,int steps, int &arrlen, vector<vector<long long>>&dp ){
+        
+//         if(indx < 0 || indx>=arrlen || steps < 0){
+//             return 0;
+//         }
+        
+//         if(indx==0 && steps==0){
+//             return 1;
+//         }
+        
+//         // if(dp[indx][steps]!=-1){
+//         //     return dp[indx][steps];
+//         // }
+        
+//         if(dp[steps][indx]!=-1){
+//             return dp[steps][indx];
+//         }
+        
+//         // cout<<indx<<" "<<steps<<" "<<arrlen<<endl; 
+//         int a=solve(indx,steps-1,arrlen,dp);
+//         int b=solve(indx+1,steps-1,arrlen,dp);
+//         int c=solve(indx-1, steps-1,arrlen,dp);
+        
+//         return dp[steps][indx]=(long long)(a%mod+b%mod+c%mod)%mod;
+//     }
+//     int numWays(int steps, int arrLen) {
+        
+//         vector<vector<long long>>dp(steps+1, vector<long long>(steps/2+1,-1));
+//         return solve(0,steps,arrLen,dp);
+//     }
+// };
+
+
 class Solution {
-public:
-    int numWays(int steps, int arrLen) {
-          int sz = min(steps / 2 + 1, arrLen);
-    vector<int> v1(sz + 2), v2(sz + 2);
-    v1[1] = 1;
-    while (steps-- > 0) {
-        for (auto i = 1; i <= sz; ++i)
-            v2[i] = ((long)v1[i] + v1[i - 1] + v1[i + 1]) % 1000000007;
-        swap(v1, v2);
+private:
+    long long solve(int steps,int arrLen, int i, vector<vector<long long>> &dp){
+        if(!i && !steps)
+            return 1;
+        if(i == arrLen || steps < 0 || i < 0 || i > steps)
+            return 0;
+        if(dp[steps][i] != -1)
+            return dp[steps][i];
+            
+        long long left = solve(steps - 1, arrLen, i - 1, dp);
+        long long right = solve(steps - 1, arrLen, i + 1, dp);
+        long long stay = solve(steps - 1, arrLen, i, dp);
+        return dp[steps][i] = (long long)((left + right + stay) % mod);
     }
-    return v1[1];
+public:
+    long long mod = 1e9 + 7;
+    int numWays(int steps, int arrLen) {
+        vector<vector<long long>> dp(steps + 1, vector<long long> (steps/2 + 1, -1));
+        return (int)solve(steps, arrLen, 0, dp);
     }
 };
