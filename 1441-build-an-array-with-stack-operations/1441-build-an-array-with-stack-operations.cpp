5839412c37dfbf448f3c0ac5b6441f6381a0d480
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-11-05 01:03:56 +0530

    Time: 4 ms (35.04%), Space: 8.4 MB (11.93%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    vector<string> buildArray(vector<int>& t, int n) {
-        vector <string> ans;
-
-        int i = 0;
-        int j = 1;
-
-        while ( i<t.size() && j<=n ) {
-
-            ans.push_back("Push");
-
-            if ( t[i] == j ) {
-                i ++;
-            }
-
-            else {
-                ans.push_back("Pop");
-            }
-
-            j++;
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<int>vis(n+1,-1);
+        
+        int indx;
+        for(int i=0;i<target.size();i++){
+            indx=target[i];
+            vis[indx]=1;
         }
-
+        
+        
+        vector<string>ans;
+        for(int i=1;i<=indx;i++){
+            if(vis[i]==-1){
+                ans.push_back("Push");
+                ans.push_back("Pop");
+            }else{
+                ans.push_back("Push");
+            }
+        }
+        
         return ans;
     }
 };
