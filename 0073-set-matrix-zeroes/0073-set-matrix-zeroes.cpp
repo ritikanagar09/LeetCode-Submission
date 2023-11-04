2c5ef99b84bf4f502bac631a9defd409d25383a3
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-11-05 01:26:55 +0530

    Time: 4 ms (98.52%), Space: 13.8 MB (41.30%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    int m,n;
-    
     void setZeroes(vector<vector<int>>& matrix) {
-        m=matrix.size();
-        n=matrix[0].size();
+        int n=matrix.size();
+        int m=matrix[0].size();
         
         vector<pair<int,int>>v;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    for(int k=0;k<m;k++){
-                        for(int b=0;b<n;b++){
-                            if(matrix[k][b]!=0 && (k==i ||b==j)){
-                                matrix[k][b]=-1e8;
-                            }
-                        }
-                    }
-                }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(matrix[i][j]==0)
+                    v.push_back({i,j});
             }
         }
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-               if(matrix[i][j]==-1e8){
-                   matrix[i][j]=0;
-               } 
+        for(int i=0;i<v.size();i++){
+            int r=v[i].first;
+            int c=v[i].second;
+            
+            for(int i=0;i<m;i++){
+                matrix[r][i]=0;
+            }
+            
+            for(int i=0;i<n;i++){
+                matrix[i][c]=0;
             }
         }
-        
-        
-        
-        
-       
     }
 };
