60b1f46ff1ecf6f6e157d82c23da4711f7b12d2a
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-11-07 20:47:36 +0530

    Time: 33 ms (37.18%), Space: 13.7 MB (42.67%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
-    int m, n;
-    
-    void bfs(int i, int j,vector<vector<char>>& grid,vector<vector<int>>&vis ){
-        vis[i][j]=1;
+    int n;
+    int m;
+    void dfs(int x, int y,vector<vector<char>>& grid, vector<vector<int>>& vis ){
         
-        // padosiyon ko visi krte hai 
         
-        queue<pair<int,int>>q;
-        q.push({i,j});
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
         
-       while (!q.empty()) {
-            auto current = q.front();
-            q.pop();
-            int x = current.first;
-            int y = current.second;
-
-            // Check all four directions
-            int dx[] = {-1, 0, 1, 0};
-            int dy[] = {0, 1, 0, -1};
-
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == '1' && !vis[nx][ny]) {
-                    vis[nx][ny] = 1;
-                    q.push({nx, ny});
-                }
+        for(int i=0;i<4;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]=='1' && !vis[nx][ny]){
+                vis[nx][ny]=1;
+                dfs(nx,ny,grid,vis);
             }
+            
         }
+        
         
     }
     int numIslands(vector<vector<char>>& grid) {
-       if (grid.empty() || grid[0].empty())
-            return 0;
-
-        queue<pair<int, int>> q;
-         m = grid.size();
-         n = grid[0].size();
-        int islands = 0;
-        vector<vector<int>> visited(m, vector<int>(n, 0));
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    islands++;
-                    // bfs(grid, i, j, visited, q, m, n);
-                    bfs(i,j,grid,visited);
+        
+        
+        n=grid.size();
+        m=grid[0].size();
+        
+        int cnt=0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    dfs(i,j,grid,vis);
+                    cnt++;
                 }
             }
         }
-        return islands;
         
+        return cnt;
     }
 };
