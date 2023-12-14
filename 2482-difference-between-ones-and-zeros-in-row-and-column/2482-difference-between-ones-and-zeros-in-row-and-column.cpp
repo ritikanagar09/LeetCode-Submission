75bc85dbf34c3841e3c62b0c351d60be0d1f7080
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-12-14 23:32:44 +0530

    Time: 199 ms (51.10%), Space: 117.5 MB (76.35%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -6,59 +6,40 @@ public:
         vector<vector<int>>ans(n,vector<int>(m,0));
         
         
-        // first cal for 1 row and 1 col 
+        vector<int>oRow(n,0);
         
+        vector<int>oCol(m,0);
         
-        // store colm wise
-        
-        for(int j=0;j<m;j++){ // colm pe iterate 
-            // ynha par row wise check kar va
-           int oCol=0;
-           int zCol=0;
-            for(int i=0;i<n;i++){// row
-                if(grid[i][j]==0){ //  colm fixed h row is changing 
-                    zCol++;
-                }
-                
-                if(grid[i][j]==1){
-                    oCol++;
-                }
-                
-                
-            }
-            
-            for(int i=0;i<n;i++){
-                ans[i][j]=(oCol -zCol);
-            }   
-        }
-        
-        
-        
-        for(int i=0;i<n;i++){ // colm pe iterate 
-            // ynha par row wise check kar va
-           int oRow=0;
-           int zRow=0;
-            for(int j=0;j<m;j++){// row
-                if(grid[i][j]==0){ //  colm fixed h row is changing 
-                    zRow++;
-                }
-                
-                if(grid[i][j]==1){
-                    oRow++;
-                }
-                
-                
-            }
-            
+        for(int i=0 ;i<n;i++){
+            int one=0; 
             for(int j=0;j<m;j++){
-                int x=(oRow-zRow);
-                int y=ans[i][j];
-                // cout<<x<<" "<<y<<endl;
-                ans[i][j]=x+y;  
-            }   
+                if(grid[i][j]==1){
+                    one++; 
+                }
+            }
+            
+            oRow[i]=one; 
         }
         
-       
+        for(int j=0 ;j<m;j++){
+            int one=0; 
+            for(int i=0;i<n;i++){
+                if(grid[i][j]==1){
+                    one++; 
+                }
+            }
+            
+            oCol[j]=one; 
+        }
+        
+        
+        for(int i=0 ;i<n;i++){
+            for(int j=0 ;j<m;j++){
+                int zCol=m-oCol[j];
+                int zRow=n-oRow[i];
+                ans[i][j]=oRow[i]+oCol[j]-zCol-zRow;
+            }
+        }
         
         return ans;
     }
