85863132a6df70e1b2c30e67f1297d639f54093e
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2023-12-16 01:30:15 +0530

    Time: 121 ms (12.28%), Space: 59.5 MB (15.61%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     long long int mod=1e9+7;
-    // int rev(int n){
-    //     string s=to_string(n);
-    //     reverse(s.begin(),s.end());
-    //     int k=stol(s);
-    //     return k;
-    // }
-     int rev(int x) {
-        int result = 0;
-        while (x > 0) {
-            result = 10 * result + (x % 10);
-            x /= 10;
-        }
-        return result;
+    int reverseString(int x){
+        string a=to_string(x);
+        
+        reverse(a.begin(),a.end());
+        int y=stoi(a);
+        return y;
     }
     int countNicePairs(vector<int>& nums) {
         int n=nums.size();
         
-        unordered_map<int,int>mpp;
-        for(int i=0;i<n;i++){
-            int k=rev(nums[i]);
-            int val=nums[i]-k;
-            mpp[val]++;
+        vector<int>rev(n);
+        
+        for(int i=0 ;i < n;i++ ){
+             int a=nums[i]; 
+             rev[i]= reverseString(a);
         }
         
-        long long int ans=0;
-        for(auto it:mpp){
+        map<long long int,long long int>mpp; 
+        
+        for(int i=0 ;i <n;i++){
+            int a=nums[i];
+            int b=rev[i] ;
+            
+            int diff= a-b;
+            // cout<<a<<" "<<b<<" "<<diff<<endl;
+            mpp[diff]++; 
+        }
+        
+        long long int cnt=0;
+        for(auto it: mpp ){
             if(it.second > 1){
-                int value=it.second;
-                long long pairs = ((1LL * value * (value - 1)) / 2LL) % mod;
-                ans= (ans + pairs) %mod;
+                long long int x=it.second;
+                cnt+= ((1LL * x*(x-1))/2LL);
             }
         }
-        return ans;
         
+        return cnt%mod;
     }
 };
