49e0acdb2077f7d082594a2b0ed3ddf990fdc316
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2024-01-02 23:45:12 +0530

    Time: 80 ms (8.35%), Space: 81.7 MB (8.49%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,64 +1,42 @@
 class Solution {
 public:
-    int solve(int i, int j , string s1,string s2){
+    
+    int solve(int i, int j, string s1, string s2,vector<vector<int>>&dp){
         
-        // base 
         
-        if(i<0){
+        // cout<<i <<" "<<j<<endl;
+        if(i < 0 && j < 0){
+            return 0;
+        }
+        
+        
+        if(i < 0 && j >= 0){ // first empty 
             return j+1;
         }
         
-        if(j<0){
+        if(j < 0 && i >= 0){
             return i+1;
         }
         
-        
-        
-        //matching 
-        if(s1[i]==s2[j]){
-            return solve(i-1,j-1,s1,s2);
-        }else{
-            
-            // int a=1+solve(i-1,j,s1,s2);
-            // int a=1+solve(i-1,j,s1,s2);
-            // int a=1+solve(i-1,j,s1,s2);
-            
-            return 1+min(solve(i-1,j,s1,s2), min(solve(i,j-1,s1,s2),solve(i-1,j-1,s1,s2)));
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
         
-        // ye alag tareeka h return krne kaa ..
-//         becuase at the end hume apni recursion call me se koi ek cheez toh return krni hai 
-        
+        if(s1[i]==s2[j]){
+            return dp[i][j]=solve(i-1,j-1,s1,s2,dp);
+        }else{
+            return dp[i][j]=min(1+solve(i-1,j-1,s1,s2,dp), min(1+solve(i-1,j,s1,s2,dp), 1+solve(i,j-1,s1,s2,dp)));
+        }
     }
     int minDistance(string word1, string word2) {
-        // return solve(word1.size(),word2.size(),word1,word2);
         
-        
-        //tabulation 
-        
-        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1));
-        
-        
-        for(int j=0;j<=word2.size();j++){
-            dp[0][j]=j;// one based indexing h islye
+        if(word1==word2){
+            return 0;
         }
+        int m=word1.size();
+        int n=word2.size();
         
-        for(int i=0;i<=word1.size();i++){
-            dp[i][0]=i;
-        }
-        
-        for(int i=1;i<=word1.size();i++){
-            for(int j=1;j<=word2.size();j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j] =dp[i-1][j-1];
-                }else{
-
-                    dp[i][j]=1+min(dp[i-1][j], min(dp[i][j-1],dp[i-1][j-1]));
-                }
-            }
-        }
-        
-        
-        return dp[word1.size()][word2.size()];
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1, word1,word2,dp);
     }
 };
