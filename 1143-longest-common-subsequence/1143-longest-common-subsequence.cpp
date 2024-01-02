e0c2f97cdf63be0ab3059580c448e99e2bf66145
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2024-01-02 22:52:31 +0530

    Time: 149 ms (5.07%), Space: 19.3 MB (70.32%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,59 +1,34 @@
 class Solution {
 public:
-    int solve(int i, int j,string str1, string str2, vector<vector<int>>&dp ){
-        if(i==0 || j==0){
+    
+    
+    int solve(int i, int j, string &s1, string &s2, vector<vector<int>>&dp){
+        
+        if(i < 0 || j< 0){
             return 0;
         }
         
-        // match or not match 
-        
-        if(dp[i][j]!=-1){
+        if(dp[i][j] != -1){
             return dp[i][j];
         }
-        
-        if(str1[i-1]==str2[j-1]){
-            return dp[i][j]=1+solve(i-1,j-1, str1,str2,dp);
+        if(s1[i]==s2[j]){
+            return dp[i][j]=1+solve(i-1,j-1,s1,s2,dp);
+        }else{
+            return dp[i][j]=max(solve(i-1,j,s1,s2,dp), solve(i,j-1,s1,s2,dp));
         }
-        
-        return dp[i][j]=max(solve(i-1,j,str1,str2,dp),solve(i,j-1,str1,str2,dp));
     }
-    int longestCommonSubsequence(string text1, string text2) {
-        
-        int m=text1.size();
-        int n=text2.size();
-        
-        // we do memoisation bcoz we have are having overlapping subproblems 
-        
-        // vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        // return solve(m,n,text1,text2,dp);
-//         
-        string str1=text1;
-        string str2=text2;
-        vector<vector<int>>dp(1000+1,vector<int>(1000+1,0));
-        
-//         int dp[1000 + 1][1000 + 1];
-        
-//         for (int i = 0; i < m + 1; i++) {
-//             for (int j = 0; j < n + 1; j++) {
-//                if (i == 0 || j == 0) {
-//                    dp[i][j] = 0;
-//                } 
-//             }
-//         }
-        
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(str1[i-1]==str2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-
-                
-            }
+    int longestCommonSubsequence(string s1, string s2) {
+        if(s1==s2){
+            return s1.size();
         }
         
-        return dp[m][n];
         
+        int m=s1.size();
+        int n=s2.size();
+        
+                vector<vector<int>>dp(m,vector<int>(n,-1));
+
+        
+        return solve(m-1,n-1,s1,s2,dp);
     }
 };
