5e4e3e09b7f9340a4097c775c8af149c921d54c8
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2024-01-24 03:29:54 +0530

    Time: 254 ms (21.92%), Space: 173.9 MB (14.19%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,53 +1,43 @@
 class Solution {
 public:
-    int maxi=0;
-    bool check(string &s1, string &s2){
-        string s= s1+s2;
-        map<char, int>m;
-        
-        for(int i=0;i<s.length();i++){
-            m[s[i]]++;
-            if(m[s[i]]>1){
-                return false;
-            }
+    
+    int solve(int indx, vector<string>&arr, string str){
+        if(indx >=arr.size()){
+            return 0;
         }
         
-        return true;
-    }
-    int solve(int i , string s, vector<string>&arr, unordered_map<string, int>&mp){
         
-        
-        if(i>=arr.size()){
-            
-            int size= s.size();
-            if(maxi<size){
-                maxi=size;
-            }
-            return maxi;
+        map<char,int>mpp;
+        bool flag1=true;
+        string curr=arr[indx];
+        string newstr=str+curr;
+        for(auto ch:newstr){
+            mpp[ch]++;
             
         }
         
+        bool flag=true;
         
-        if(mp.find(s) != mp.end()) return mp[s];
-        
-        int notTake= solve(i+1,s, arr, mp);
-        
-        int take=-1;
-        if(check(s,arr[i])){
-            take=solve(i+1, s+arr[i],arr,mp);
+        for(auto it:mpp){
+            if(it.second > 1){
+                flag=false;
+            }
         }
         
-        return max(take, notTake);
+        int take=0;
+        if(flag && flag1){
+            take=curr.size() +solve(indx+1,arr, str+curr);
+        }
+        
+        int nottake=solve(indx+1,arr,str);
+        
+        return max(take,nottake);
+        
+        
     }
-    
-    
     int maxLength(vector<string>& arr) {
-        string s="";
         
-        //vector<int>dp( arr.size()+1,-1);
-        unordered_map<string, int>mp;
-        return solve(0, s, arr,mp);
-        
-        
+        string str="";
+        return solve(0,arr,str);
     }
 };
