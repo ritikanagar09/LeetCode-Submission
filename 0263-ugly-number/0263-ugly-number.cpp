3077c5d3545e6b0c60d52dd6be1757adbe828e28
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2024-03-11 12:49:22 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (22.04%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,49 +1,18 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n == 0)
-        {
+        if(n <= 0){
             return false;
         }
-        if(n == 2 || n == 3 ||n ==5)
-        {
-            return true;
+        
+        while( n > 1){
+            
+            if(n%2==0) n=n/2;
+            else if(n%3==0) n=n/3;
+            else if(n%5==0)n=n/5;
+            else break;
         }
-
-
-        while(n%2 == 0)
-        {
-            n/=2;
-        }
-
-        if( n == 1)
-        {
-            return true;
-        }
-
-        while(n%3 == 0)
-        {
-            n/=3;
-        }
-
-        if( n== 1)
-        {
-            return true;
-        }
-
-
-        while(n%5 == 0)
-        {
-            n/=5;
-        }
-
-        if(n==1)
-        {
-            return true;
-        }
-
-
-
-        return false;
+        
+        return n==1;
     }
 };
