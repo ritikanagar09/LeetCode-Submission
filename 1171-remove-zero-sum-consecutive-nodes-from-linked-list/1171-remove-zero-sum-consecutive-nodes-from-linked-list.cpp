a73f2cd787138fbc67eb2f61e7f26bc6eb447d60
Ritika Nagar <87242161+ritikanagar09@users.noreply.github.com>
2024-03-14 00:13:08 +0530

    Time: 12 ms (24.25%), Space: 15.1 MB (16.98%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -11,26 +11,37 @@
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
-        ListNode* dummy= new ListNode(0,head);
-        ListNode* start=dummy;
+        ListNode* dummy= new ListNode(0,head);// it is pointing head's next 
         
-        while(start != NULL){
-            
-            int psum=0;
-            ListNode* end=start->next;
-            
-            while(end != NULL){
-                psum+=end->val;
-                if(psum == 0){
-                    start->next=end->next;
+        ListNode* curr=dummy;
+        int psum=0;
+        unordered_map<int,ListNode*>mpp;
+        
+        while(curr != NULL){
+            psum+=curr->val;
+            if(mpp.find(psum) != mpp.end()){
+                ListNode* start=mpp[psum];
+                
+                // dont forget to delete the nodes from map
+                curr=start->next;
+                
+                int sum=psum+curr->val;
+                while(sum != psum){ // 6 --> 3 
+                    mpp.erase(sum);
+                    curr=curr->next;
+                    sum+=curr->val;
                 }
-                end=end->next;
+                
+                
+                 start->next=curr->next;
+                
+            }else{
+                mpp[psum]=curr;
             }
             
-            start=start->next;
+            curr=curr->next;
         }
-        
-        
         return dummy->next;
+        
     }
 };
